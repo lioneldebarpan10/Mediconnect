9d149d3f789f497a419bf2b55425ce7848aa6e16
Debarpan Deb <dbrpn07cse@gmail.com>
2026-01-29 22:54:47 +0530

    Frontend Structure Config and Pages structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+
+    </div>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaUserDoctor } from 'react-icons/fa6'
-import {Navlink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -9,52 +9,59 @@ const Navbar = () => {
       <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
 
          {/*Adding Mediconnect Logo */}
-         <a href="#">
-            <FaUserDoctor />
-
+         <a href="#" style={{ display: 'flex', alignItems: 'center', fontSize: '1.2rem' }}>
+            <FaUserDoctor style={{ marginRight: '8px' }} /> MediConnect
          </a>
 
          {/*Creating Navlinks */}
-         <ul>
-            <Navlink to='/'>
+         <ul className='md:flex items-start gap-5 font-medium hidden'>
 
-               <li>HOME</li>
-               <hr />
+            <NavLink to='/'>
+               <li className='py-1'>HOME</li>
+               <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
 
-            </Navlink>
-            <Navlink to='/doctors'>
+            <NavLink to='/doctors'>
+               <li className='py-1'>ALL DOCTORS</li>
+               <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink >
 
-               <li>ALL DOCTORS</li>
-               <hr />
+            <NavLink to='/about'>
+               <li className='py-1'>ABOUT</li>
+               <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
 
-            </Navlink >
-            <Navlink to='/about'>
+            <NavLink to='/contact'>
+               <li className='py-1'>CONTACT</li>
+               <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
 
-               <li>ABOUT</li>
-               <hr />
-
-            </Navlink>
-            <Navlink to='/contact'>
-
-               <li>CONTACT</li>
-               <hr />
-
-            </Navlink>
          </ul>
 
          {/*Create account Button for Mediconnect */}
 
-         <div>
-            <button>Create Account</button>
+         <div className='flex items-center gap-4'>
+            <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            <img src="" alt="" className='w-6 md:hidden' />
 
             {/*Mobile Menu */}
 
+            <div>
+               <div>
+                  <a href=""></a>
+                  <img src="" alt="" />
+               </div>
 
+               <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                  <NavLink to='/'><p className='px-4 py-2 rounded full inline-block'>HOME</p></NavLink>
+                  <NavLink to='/doctors'><p className='px-4 py-2 rounded full inline-block'>ALL DOCTORS</p></NavLink>
+                  <NavLink to='/about'><p className='px-4 py-2 rounded full inline-block'>ABOUT</p></NavLink>
+                  <NavLink to='contact'><p className='px-4 py-2 rounded full inline-block'>CONTACT</p></NavLink>
+               </ul>
+            </div>
          </div>
-
-         
-
-
       </div>
    )
 }
+
+export default Navbar
