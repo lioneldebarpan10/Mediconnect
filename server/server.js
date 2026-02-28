a44ab00628962b2491815adf3bd189a12b4d71e4
Debarpan Deb <dbrpn07cse@gmail.com>
2026-03-01 02:56:35 +0530

    config of cloudinary & mongoose and create doctors and user model

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'; // type module
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors()); // allow frontend to connect with backend
+
+// api endpoint
+app.get('/' , (req , res)=> {
+   res.send('Api is Working on PORT 4000');
+})
+
+// Listening on PORT 4000
+app.listen(port , () => {
+   console.log("Server is running on PORT",port);
+})
